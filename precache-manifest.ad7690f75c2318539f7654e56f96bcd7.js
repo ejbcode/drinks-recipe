941a0710c118ad56474a7cb24ecc027616af52d8
Ernesto Briceño <ejbcode@gmail.com>
2020-07-27 18:23:23 -0400

    Updates

---
+++ precache-manifest.ad7690f75c2318539f7654e56f96bcd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27110f54336328d964658c5813f2e31c",
+    "url": "/drinks-recipe/index.html"
+  },
+  {
+    "revision": "cf73d395409311e2a160",
+    "url": "/drinks-recipe/static/css/main.5f74ccce.chunk.css"
+  },
+  {
+    "revision": "5b78ab6f65be96239a88",
+    "url": "/drinks-recipe/static/js/2.64e8f1db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drinks-recipe/static/js/2.64e8f1db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf73d395409311e2a160",
+    "url": "/drinks-recipe/static/js/main.684ad37e.chunk.js"
+  },
+  {
+    "revision": "b0cd543badf412e2d486",
+    "url": "/drinks-recipe/static/js/runtime-main.52529762.js"
+  }
+]);
